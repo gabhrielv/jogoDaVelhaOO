2d4036059c20608b436873b1da1ae77247e2303f
Home <Home@DESKTOP-7G9LTTC>
2024-07-30 13:34:30 -0300

    cria jogador e tabuleiro java

---
+++ src/main/java/com/mycompany/mavenproject2/jogador.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Home
+ */
+public class jogador {
+    private char simbolo;
+    private int jogada[][];
+    public jogador(char simbolo, int[][] jogada) {
+        this.simbolo = simbolo;
+        this.jogada = jogada;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/mavenproject2/tabuleiro.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author ice
+ */
+public class tabuleiro {
+    private char[][] posicoes;
+    private int jogadas;
+    
+    
+    
+    public void getPosicoes() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(this.posicoes[i][j] + " ");
+            }
+            System.out.println(); // Mudança de linha após cada linha da matriz
+        }
+    }
+    
+    public tabuleiro() {
+        this.posicoes = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.posicoes[i][j] = ' '; // Inicializa com espaço em branco
+            }
+        }
+        this.jogadas = 0;
+    }
+    
+    public void jogada(char simb, int col, int lin){
+        if(this.posicoes[col][lin]== ' '){
+            this.posicoes[col][lin]=simb;
+            this.jogadas++;
+        } else {
+            System.out.println("Posição já está ocupada!");
+        }
+        this.verificaJogo();
+    }
+
+    private void verificaJogo() {
+        for (int i = 0; i < 3; i++) {
+            if(this.posicoes[i][0]==this.posicoes[i][1] && this.posicoes[i][2]==this.posicoes[i][1]){
+                System.out.println("Fim de jogo!");
+                i=10;
+                return;
+            }            
+        }
+        for (int i = 0; i < 3; i++) {
+            if(this.posicoes[0][i]==this.posicoes[1][i] && this.posicoes[2][i]==this.posicoes[1][i]){
+                System.out.println("Fim de jogo!");
+                return;
+            }            
+        }
+            if(this.posicoes[0][0]==this.posicoes[1][1] && this.posicoes[1][1]==this.posicoes[2][2]){
+                System.out.println("Fim de jogo!");
+                return;
+            }    
+            if(this.posicoes[0][2]==this.posicoes[1][1] && this.posicoes[1][1]==this.posicoes[2][0]){
+                System.out.println("Fim de jogo!");
+                return;
+            }
+        if(this.jogadas>=9){
+                System.out.println("Empate!");
+        }
+    }
+}
